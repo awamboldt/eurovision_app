2c0a54ad311d43f19d47e5455670d49574815a1d
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-04-22 10:12:52 +0200

    added neringas pipeline image

--- home.py
+++ home.py
@@ -58,7 +58,17 @@ def show_home():
         </p>
         </div>""", unsafe_allow_html=True
     )
+ # image
+    col1, col2, col3 = st.columns(3)
 
+    with col1:
+        st.write(' ')
+
+    with col2:
+        st.image('./data/pipeline_image.png', width=1000)
+
+    with col3:
+        st.write(' ')
 
     # give some empty spaces in between
     st.markdown("<br><br>", unsafe_allow_html=True)
