94409c2c8a960af078f8be793cbf7ed41da0adfd
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-04-22 14:03:49 +0200

    last won map not zooming fix

--- fun.py
+++ fun.py
@@ -295,7 +295,7 @@ def show_fun():
     with open(path_to_html,'r') as f: 
         html_data = f.read()
 
-    st.components.v1.html(html_data, scrolling=True, height=1000, width = 1600)
+    st.components.v1.html(html_data, scrolling=True, height=1000, width = 1400)
 
 
 
